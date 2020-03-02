05c1be0ea2babc0720f6ccc7d8607042c027c0c8
tmralmeida <tm.almeida@ua.pt>
2020-03-02 18:25:32 +0000

    uncomment

--- src/panorama.py
+++ src/panorama.py
@@ -4,48 +4,6 @@ import cv2
 import numpy as np
 import imutils
 
-
-# class Stitcher:
-#     def __init__(self):
-#         self.cachedH = None
-
-#     def stitch(self, images, ratio=0.75, reprojThresh=4.0):
-#         (imageB, imageA) = images
-#         if self.cachedH is None:
-#             (kpsA, featuresA) = self.detectAndDescribe(imageA)
-#             (kpsB, featuresB) = self.detectAndDescribe(imageB)
-#             M = self.matchKeypoints(kpsA, kpsB,featuresA, featuresB, ratio, reprojThresh)
-#             if M is None:
-# 				return None
-#             self.cachedH = M[1]
-#         result = cv2.warpPerspective(imageA, self.cachedH,(imageA.shape[1] + imageB.shape[1], imageA.shape[0]))
-#         result[0:imageB.shape[0], 0:imageB.shape[1]] = imageB
-      
-#         return result
-    
-#     def detectAndDescribe(self, image):
-#         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#         detector = cv2.ORB_create()
-#         kps = detector.detect(gray, None)
-#         (kps,features) = detector.compute(gray, kps)
-#         kps = np.float32([kp.pt for kp in kps])
-#         return (kps,features) 
-    
-#     def matchKeypoints(self, kpsA, kpsB, featuresA, featuresB, ratio, reprojThresh):
-#         matcher = cv2.DescriptorMatcher_create("BruteForce")
-#         rawMatches = matcher.knnMatch(featuresA, featuresB, 2)
-#         matches = []
-#         for m in rawMatches:
-#             if len(m) == 2 and m[0].distance < m[1].distance * ratio:
-#                 matches.append((m[0].trainIdx, m[0].queryIdx))
-#         if len(matches) > 4:
-#             ptsA = np.float32([kpsA[i] for (_, i) in matches])
-#             ptsB = np.float32([kpsB[i] for (i, _) in matches])
-#             (H, status) = cv2.findHomography(ptsA, ptsB, cv2.RANSAC,reprojThresh)
-#             return (matches, H, status)
-#         return None
-
-
 # input:images
 
 
